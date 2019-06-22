5231bfc5a941dbe31e5eb0e26114420494ae36ce
hendisantika <hendisantika@yahoo.co.id>
2019-06-22 22:07:10 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootunittest/SpringbootUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootunittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUnitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUnitTestApplication.class, args);
+    }
+
+}
