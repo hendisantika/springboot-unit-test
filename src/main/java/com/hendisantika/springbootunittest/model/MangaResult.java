85f46114411b876a592f88adc2c22f0320b1c0f3
hendisantika <hendisantika@yahoo.co.id>
2019-06-23 19:37:48 +0700

    #4 - Add Manga Result Model

---
+++ src/main/java/com/hendisantika/springbootunittest/model/MangaResult.java
@@ -0,0 +1,25 @@
+package com.hendisantika.springbootunittest.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-unit-test-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-23
+ * Time: 19:37
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class MangaResult {
+
+    private List<Manga> result;
+
+}
