b72d893b7fa90c92436b8107cc1f6fa59ff6c49d
hendisantika <hendisantika@yahoo.co.id>
2019-06-26 05:43:05 +0700

    #11 - Add testSearchSync in MangaControllerIntegrationTest in integration

--- src/test/java/com/hendisantika/springbootunittest/integration/MangaControllerIntegrationTest.java
+++ src/test/java/com/hendisantika/springbootunittest/integration/MangaControllerIntegrationTest.java
@@ -2,13 +2,20 @@ package com.hendisantika.springbootunittest.integration;
 
 import com.hendisantika.springbootunittest.controller.MangaController;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 /**
@@ -37,5 +44,12 @@ public class MangaControllerIntegrationTest {
         this.mockMvc = standaloneSetup(this.mangaController).build();// Standalone context
         // mockMvc = MockMvcBuilders.webAppContextSetup(wac)
         // .build();
+    }
+
+    @Test
+    public void testSearchSync() throws Exception {
+        mockMvc.perform(get("/manga/sync/ken").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*.title", hasItem(is("Hokuto no Ken"))));
     }
 }
