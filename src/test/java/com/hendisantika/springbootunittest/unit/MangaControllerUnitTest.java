1d4e5b0c1e766b97c1b729cb753e5b4c616c9fcb
hendisantika <hendisantika@yahoo.co.id>
2019-06-26 05:47:28 +0700

    #16 - Add before setup MangaControllerUnitTest in integration

--- src/test/java/com/hendisantika/springbootunittest/unit/MangaControllerUnitTest.java
+++ src/test/java/com/hendisantika/springbootunittest/unit/MangaControllerUnitTest.java
@@ -3,6 +3,7 @@ package com.hendisantika.springbootunittest.unit;
 import com.hendisantika.springbootunittest.controller.MangaController;
 import com.hendisantika.springbootunittest.model.Manga;
 import com.hendisantika.springbootunittest.service.MangaService;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +12,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 /**
  * Created by IntelliJ IDEA.
@@ -38,4 +42,24 @@ public class MangaControllerUnitTest {
      * List of samples mangas
      */
     private List<Manga> mangas;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = standaloneSetup(this.mangaController).build();// Standalone context
+        // mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+        // .build();
+        Manga manga1 = Manga.builder()
+                .title("Hokuto no ken")
+                .description("The year is 199X. The Earth has been devastated by nuclear war...")
+                .build();
+        Manga manga2 = Manga.builder()
+                .title("Yumekui Kenbun")
+                .description("For those who suffer nightmares, help awaits at the Ginseikan Tea House, where patrons can order much more than just Darjeeling. Hiruko is a special kind of a private investigator. He's a dream eater....")
+                .build();
+
+        mangas = new ArrayList<>();
+        mangas.add(manga1);
+        mangas.add(manga2);
+    }
+
 }
